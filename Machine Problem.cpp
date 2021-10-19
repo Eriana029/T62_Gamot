ae276f2f991c2de7e6729881f665ccfc4c673e2c
LADY <80914127+Eriana029@users.noreply.github.com>
2021-10-19 22:23:56 +0800

    Add files via upload

---
+++ Machine Problem.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n, k=1;
+    cout << "Input Any Positive Integers: ";
+    cin >> n;
+
+    while (n != 1){
+
+        if (n%2 == 0)
+            n = n/2;
+
+        else
+            n = n*3+1;
+
+        cout << "\nCurrent Given Value is " << n;
+        k++;
+    }
+    return 0;
+}
